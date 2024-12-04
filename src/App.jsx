1d0add8069dec7f6de377c687b403dc40da83f69
CrislayneSoares <crissoaresr@hotmail.com>
2024-12-03 22:52:50 -0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { BrowserRouter } from "react-router-dom" 
+import { BrowserRouter, Route, Routes } from "react-router-dom" 
+import Home from "./pages/Home/Home"
+import LivrosDoados from "./pages/LivrosDoados/LivrosDoados"
+import QueroDoar from "./pages/QueroDoar/QueroDoar"
+import Header from "./components/header/header"
+import Footer from "./components/footer/footer"
 function App() {
   return (
     <>
       <BrowserRouter>
-        
+        <Header></Header>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/LivrosDoados" element={<LivrosDoados/>}/>
+          <Route path="/QueroDoar" element={<QueroDoar/>}/>
+        </Routes>
+
+        <Footer></Footer>
       </BrowserRouter>
     </>
   )
